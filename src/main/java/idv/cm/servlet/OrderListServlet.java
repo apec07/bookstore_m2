1d9ee34ee0d431e92a3f58c581a900089bb8cbe3
apec07 <apec07@gmail.com>
2021-02-25 16:30:55 +0800

    modify orderedlist

--- src/main/java/idv/cm/servlet/OrderListServlet.java
+++ src/main/java/idv/cm/servlet/OrderListServlet.java
@@ -1,12 +1,13 @@
 package idv.cm.servlet;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Collections;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,17 +16,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.apache.logging.log4j.Logger;
+import org.apache.jasper.tagplugins.jstl.core.ForEach;
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import com.google.gson.Gson;
 
 import idv.cm.db.BookVO;
 import idv.cm.db.ConnectionFactory;
+import idv.cm.db.OrderListVO;
 import idv.cm.db.OrderedListDTO;
 import idv.cm.db.OrderedListVO;
 import idv.cm.db.UserVO;
-import idv.cm.listener.Log4jServletContextListener;
 
 /**
  * Servlet implementation class OrderListServlet
@@ -35,7 +37,7 @@ public class OrderListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private Connection con = null;
 	private Gson gson = null;
-	private Logger logger ;
+	private Logger logger =null;
 
 	/**
 	 * @see HttpServlet#HttpServlet()
@@ -50,14 +52,14 @@ public class OrderListServlet extends HttpServlet {
 	 */
 	public void init(ServletConfig config) throws ServletException {
 		// DB connection init()
-		System.out.println("Shopping Servlet init()!");
-		// from listener
-		logger = (Logger)getServletContext().getAttribute("log4j2");
-		
-		
+//		System.out.println("Shopping Servlet init()!");
+		logger = LogManager.getLogger();
+		logger.traceEntry();
+		logger.info("Shopping Servlet init()");
 		gson = new Gson();
 		// DB connection init()
 		ConnectionFactory factory = ConnectionFactory.getInstance();
+		logger.traceExit();
 		try {
 			con = factory.getConnection();
 		} catch (SQLException e) {
@@ -71,10 +73,14 @@ public class OrderListServlet extends HttpServlet {
 	 */
 	public void destroy() {
 		// DB connection close()
-		System.out.println("Shopping Servlet destory()!");
+//		System.out.println("Shopping Servlet destory()!");
+		logger.traceEntry();
+		logger.info("Shopping Servlet destory()");
 		// release connection
 		con = null;
 		gson = null;
+		logger.traceExit();
+		logger = null;
 	}
 
 	private List<OrderedListVO> getOrderList(HttpServletRequest req,UserVO user) {
@@ -89,16 +95,26 @@ public class OrderListServlet extends HttpServlet {
 			throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 		response.setContentType("text/html; charset=utf-8");
-		logger.entry();
+		logger.traceEntry();
 		UserVO user = (UserVO) request.getSession().getAttribute("user");
 		PrintWriter out = response.getWriter();
-		out.println("Hi, "+user.getName()+" ,Here is yor ordered list - ");
-		out.println(getOrderList(request,user));
-		logger.info("test LOGGER");
-		logger.info(gson.toJson(getOrderList(request,user)));
-		request.getServletContext().log("----------------------------------");
-		request.getServletContext().log(gson.toJson(getOrderList(request,user)));
-		logger.exit();
+		
+		//logger.debug(gson.toJson(getOrderList(request,user)));
+		List<OrderedListVO> list = getOrderList(request,user);
+		//out.println("Hi, "+user.getName()+" ,Here is yor ordered list - ");
+		//out.println(list);
+		request.getSession().setAttribute("session_Order", list);
+		request.setAttribute("req_Order", list);
+		logger.info(gson.toJson(list));
+		List<String> uu = Collections.list(request.getAttributeNames());
+		for (String s : uu) {
+			logger.info("attributeName - "+ s);
+		}
+		logger.traceExit();
+		//RequestDispatcher rd = getServletContext().getRequestDispatcher("/orderlist.jsp");
+		//rd.forward(request, response);
+		response.sendRedirect(request.getContextPath()+"/orderlist.jsp");
+		
 	}
 
 	/**
